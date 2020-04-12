563a67516d4671ee1614ca3adc92f7ebbeb38747
Danilo Cianfrone <danilocianfr@gmail.com>
2020-04-12 20:43:41 +0200

    listener: add Sink implementation

--- listener/sink.go
+++ listener/sink.go
@@ -7,45 +7,72 @@ import (
 	"github.com/streadway/amqp"
 )
 
+// Sink allows for listening from multiple Listeners, by maintaining
+// an amqp.Delivery sink to which all the messages are sent.
+//
+// Returns nil if no listeners are supplied.
 func Sink(listeners ...Listener) Listener {
 	if len(listeners) == 0 {
 		return nil
 	}
 
-	return Func(func(conn *amqp.Connection, ch *amqp.Channel) (<-chan amqp.Delivery, error) {
-		deliveries := make([]<-chan amqp.Delivery, 0, len(listeners))
+	sinker := new(sinker)
+	sinker.listeners = listeners
 
-		for _, listener := range listeners {
-			delivery, err := listener.Listen(conn, ch)
-			if err != nil {
-				return nil, fmt.Errorf("listener.Sink: failed to listen, %w", err)
-			}
-
-			deliveries = append(deliveries, delivery)
-		}
-
-		wg := new(sync.WaitGroup)
-		wg.Add(len(deliveries))
-
-		sink := make(chan amqp.Delivery, 1)
-		for _, delivery := range deliveries {
-			go sinkFromSource(wg, sink, delivery)
-		}
-
-		go func() {
-			wg.Wait()
-			close(sink)
-		}()
-
-		return sink, nil
-	})
+	return sinker
 }
 
-func sinkFromSource(wg *sync.WaitGroup, sink chan<- amqp.Delivery, source <-chan amqp.Delivery) {
-	for delivery := range source {
-		fmt.Println("sinking delivery from", delivery.ConsumerTag)
-		sink <- delivery
+type sinker struct {
+	sync.WaitGroup
+
+	listeners  []Listener
+	deliveries []<-chan amqp.Delivery
+}
+
+func (sinker *sinker) Listen(conn *amqp.Connection, ch *amqp.Channel) (<-chan amqp.Delivery, error) {
+	if err := sinker.deliveriesSinks(conn, ch); err != nil {
+		return nil, err
 	}
 
-	wg.Done()
+	return sinker.spawnSinkersFromDeliveries(), nil
+}
+
+func (sinker *sinker) deliveriesSinks(conn *amqp.Connection, ch *amqp.Channel) error {
+	deliveries := make([]<-chan amqp.Delivery, 0, len(sinker.listeners))
+
+	for _, listener := range sinker.listeners {
+		delivery, err := listener.Listen(conn, ch)
+		if err != nil {
+			return fmt.Errorf("listener.Sink: failed to listen, %w", err)
+		}
+
+		deliveries = append(deliveries, delivery)
+	}
+
+	sinker.deliveries = deliveries
+
+	return nil
+}
+
+func (sinker *sinker) spawnSinkersFromDeliveries() <-chan amqp.Delivery {
+	sinker.Add(len(sinker.deliveries))
+	sink := make(chan amqp.Delivery, len(sinker.deliveries))
+
+	for _, delivery := range sinker.deliveries {
+		go sinker.sink(sink, delivery)
+	}
+
+	go func() {
+		sinker.Wait()
+		close(sink)
+	}()
+
+	return sink
+}
+
+func (sinker *sinker) sink(sink chan<- amqp.Delivery, source <-chan amqp.Delivery) {
+	defer sinker.Done()
+	for delivery := range source {
+		sink <- delivery
+	}
 }
