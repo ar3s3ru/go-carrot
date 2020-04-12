52b62dce2018c3742ef2edf611eb0d70f5287fd3
Danilo Cianfrone <danilocianfr@gmail.com>
2020-04-12 20:34:42 +0200

    kind: add documentation

--- topology/exchange/kind/kind.go
+++ topology/exchange/kind/kind.go
@@ -1,7 +1,12 @@
+// Package kind contains all supported AMQP exchange kinds.
 package kind
 
+// Kind represents an AMQP exchange kind.
 type Kind string
 
+// Supported AMQP exchange kinds.
+//
+// For more information, please visit https://www.rabbitmq.com/tutorials/amqp-concepts.html#exchanges.
 const (
 	Direct   Kind = "direct"
 	Fanout   Kind = "fanout"
